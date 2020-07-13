1f0a88fe867a6ff3517fa9714bbe45a060a068b7
unknown <kwakms821@daum.net>
2020-07-13 23:05:26 +0900

    Add readme, resize function

--- Readme.md
+++ Readme.md
@@ -1,18 +1,19 @@
-# 2020 WEB DEVELOPMENT
+# WEB DEVELOPMENT
 
-이 문서는 웹 개발을 이해하기 위해 작성하는 파일입니다.
-해당 문서의 저작권은 아래 출처에 있으며, 이해를 위해 작성하는 파일임으로 오역이 있을 수 있습니다.
-개인적인 공부를 위해 작성하는 파일로 반복되는 구문이 있을 수 있습니다.
+이 문서는 웹 개발에 필요한 부분을 정리한 문서입니다.
+상시 업데이트 됩니다.
 
-## 출처 / 참고
+## 목차
 
-- [Modern JavaScript Tutorial](https://poiemaweb.com/)에 있음을 밝힙니다.
-- [### <처음 만나는 자바스크립트> 리포지토리 바로 가기 ###](https://github.com/Jpub/FirststepJavascript)
-
-## 목록
-
-- 시멘틱 웹
-- display 속성 및 배치
-- selecotr 선택
-- 말 줄임
-- js 기본개념
+- jQuery each
+- function
+- display (flex 포함)
+- git
+- horizontal align
+- image
+- rectangular
+- resize
+- sementic web
+- typo graphy
+- vendor prefix
+- white wrap
---
+++ resize.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    //브라우저 로딩 시
+    rwd();
+
+    //브라우저 로딩 시 rwd 호출
+    $(window).on('resize',function(){
+        rwd();
+    })
+
+    function rwd(){
+        var wid = $(window).width();
+
+        if(wid >=1000){
+            $('#header').on('mouseenter',function(e){
+                e.preventDefault();
+                $('#gnb ul').show();
+                $('.bgGnb').show();
+            })
+            $('#header').on('mouuseleave',function(e){
+                e.preventDefault();
+                $('#gnb ul').hide();
+                $('.bgGnb').hide();
+            })
+        }else{
+            $('#header').off('mouseenter');
+            $('#header').off('mouseleave');
+        }
+    }
+
+    $('.btnCall').on("click",function(){
+        var isOpen = $("gnbMo").hasClass("on");
+
+        if(isOpen){
+            $("#gnbMo").removeClass("on");
+        }else{
+            $("#gnbMo").addClass("on");
+        }
+    });
+})
