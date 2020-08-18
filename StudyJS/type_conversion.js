50ba926466d838cc8376d5e817bf4a291e503fc2
unknown <kwakms821@daum.net>
2020-08-18 23:49:28 +0900

    type_conversion #1

---
+++ StudyJS/type_conversion.js
@@ -0,0 +1,19 @@
+/*
+함수와 연산자에 전달되는 값은 대부분 적절한 자료형으로 자동 변환됩니다.
+이런 과정을 "형 변환(type conversion)"이라고 합니다.
+*/
+
+/*
+문자형으로 변환
+
+alert메서드는 매개변수로 문자형을 받기 때문에,
+alert(value)에서 value는 문자형이어야 합니다.
+만약, 다른 형의 값을 전달받으면 이 값은 문자형으로 자동 변환됩니다.
+*/
+String(value) 함수를 호출해 전달받은 값을 문자열로 변환 할 수도 있습니다.
+
+let value = true;
+console.log(typeof value); // boolean
+
+value = String(value); // 변수 value엔 문자열 "true"가 저장됩니다.
+console.log(typeof value); // string
