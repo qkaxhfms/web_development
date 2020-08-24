c06d96562bed3c2a0162187a63d17cdb1b45c03f
unknown <kwakms821@daum.net>
2020-08-24 23:33:10 +0900

    arrow_function #1

---
+++ StudyJS/arrow_function.js
@@ -0,0 +1,20 @@
+// Arrow Function
+
+// 함수 표현식
+function sayHi(){
+    alert("hello");
+}
+
+sayHi();
+
+// 함수 선언식
+let sayHi2 = function(){
+    alert("hello");
+}
+
+sayHi2();
+
+/*
+함수 선언문과 함수 선언식의 차이
+함수 선언문은 호이스팅이 일어나고 함수 표현식은 호이스팅이 일어나지 않는다.
+*/
