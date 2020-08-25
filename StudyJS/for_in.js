14bd49bcca3937fd4148a76e1da967df66a8dbc4
unknown <kwakms821@daum.net>
2020-08-25 23:22:23 +0900

    for in

---
+++ StudyJS/for_in.js
@@ -0,0 +1,22 @@
+//for_in
+
+const object = { a: 1, b: 2, c: 3 };
+
+for (const property in object) {
+  console.log(`${property}: ${object[property]}`);
+}
+
+// expected output:
+// "a: 1"
+// "b: 2"
+// "c: 3"
+
+const example = [1,2,3];
+
+for(var i =0; i<example.length; i++){
+    console.log(i);
+}
+
+for(var i in example){
+    console.log(i);
+}
