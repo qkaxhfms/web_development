177b268fe50a77e83c46797049c1c16f37fe45c1
MS\kwakm <kwakms821@daum.net>
2020-12-23 14:49:32 +0900

    calcuator 생성

---
+++ calculator/common.js
@@ -0,0 +1,7 @@
+// 구구단
+for (start = 2; start < 10; start++) {
+    for (startSecond = 1; startSecond < 10; startSecond++) {
+        result = start + "x" + startSecond + "=" + start * startSecond + "<br>";
+        document.write(result);
+    }
+}
